e0aa799f90831ebf08f822c544bf5dff702b6647
Koung Buntha <buntha.k@ltlabs.co>
2023-01-18 12:48:33 +0700

    Update register function

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,15 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'phone',
-        'photo',
-        'provider',
-        'otp_code',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.

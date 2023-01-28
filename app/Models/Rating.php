acd8ae30bfe77d2058f1c7f5ba3ef99a37ade561
Koung Buntha <buntha.k@ltlabs.co>
2023-01-28 18:57:33 +0700

    Update 28th

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -10,4 +10,21 @@ class Rating extends Model
     use HasFactory;
     protected $guarded = [];
     protected $table = 'ratings';
+    protected $appends = array(
+        'name', 
+        'photo', 
+    );
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+    public function getNameAttribute()
+    {
+        return $this->user->name; 
+    }
+    public function getPhotoAttribute()
+    {
+        return $this->user->photo; 
+    }
+
 }
